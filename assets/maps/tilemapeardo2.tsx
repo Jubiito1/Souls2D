6153e420d157ea788ead7b9c8439da0f14bfa25d
Jubiito1 <bollingerjulianguido@gmail.com>
2025-11-04 21:34:07 -0300

    mapa nuevo cargado

---
+++ assets/maps/tilemapeardo2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemapeardo2" tilewidth="32" tileheight="32" spacing="1" tilecount="144" columns="12">
+ <image source="tilemapeardo2.png" width="400" height="400"/>
+</tileset>
---
+++ lwjgl3/build/resources/main/maps/tilemapeardo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemapeardo" tilewidth="32" tileheight="32" spacing="1" tilecount="225" columns="15">
+ <image source="tilemapeardo.png" width="500" height="500"/>
+</tileset>
